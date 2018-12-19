a7c68cb30c9d376db9f8124ff1c7c175f5b3eb4a
milespucarelli <miles.pucarelli@gmail.com>
2018-12-19 14:05:16 -0500

    Done

--- solution.rb
+++ solution.rb
@@ -1,1 +1,23 @@
+require 'pry'
 
+def palindrome?(str)
+  str == str.reverse
+end
+
+# def palindrome?(str)
+#   index = 0
+#   reverse_index = str.length - 1
+#
+#   check = true
+#   until index >= reverse_index || check
+#     if str[index] != str[reverse_index]
+#       check = false
+#     else
+#       index += 1
+#       reverse_index -= 1
+#     end
+#   end
+#   check
+# end
+
+binding.pry
